42409dd84fcaaef4de13d8a571c18261f8ba1c70
Murad <saratansky@gmail.com>
2021-10-23 18:13:32 +0300

    инициализировал

---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Info extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h2>Wandex wather</h2>
+				<hi>Погода в твоем регионе</hi>
+			</div>
+		);
+	}
+}
+
+export default Info
